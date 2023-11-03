7f5c221fd25da00a6814a551daa327450e4743ff
Heikki Pulli <heikki.pulli@helsinki.fi>
2023-11-03 10:02:40 +0200

    The changes made in this commit include: - Added `middleman/models/llm_model.py` - Modified `middleman/models/llm_model.py` to include a new class `HuggingfaceConversationalRetrievalModel` with various methods for setting up the model and its components. - Deleted no files.

--- middleman/models/llm_model.py
+++ middleman/models/llm_model.py
@@ -1,5 +1,61 @@
+from typing import Dict, Any
 from langchain.chat_models import ChatOpenAI
 from langchain.llms import HuggingFaceHub
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+
+from middleman.utils.utils import (
+    extract_text_from_PDF,
+    save_chunks_into_vectorstore,
+    split_content_into_chunks,
+)
+
+
+class HuggingfaceConversationalRetrievalModel:
+    repo_id: str
+    model_kwargs: Dict
+    llm: HuggingFaceHub
+    memory: ConversationBufferMemory
+    chain: ConversationalRetrievalChain
+    vector_store: FAISS
+    embedding: HuggingFaceInstructEmbeddings
+    files: Any
+
+    def __init__(self, model_kwargs, repo_id="mistralai/Mistral-7B-v0.1") -> None:
+        self.repo_id = repo_id
+        self.model_kwargs = model_kwargs
+        self.embedding = HuggingFaceInstructEmbeddings()
+
+    def set_files(self, files):
+        self.files = files
+        return self
+
+    def set_llm(self):
+        self.llm = HuggingFaceHub(repo_id=self.repo_id, model_kwargs=self.model_kwargs)
+        return self
+
+    def set_conversation_buffer_memory(self):
+        self.memory = ConversationBufferMemory(
+            memory_key="chat_history", return_messages=True
+        )
+        return self
+
+    def set_chain(self):
+        self.chain = ConversationalRetrievalChain.from_llm(
+            llm=self.llm,
+            retriever=self.vector_store.as_retriever(),
+            memory=self.memory,
+            verbose=True,
+        )
+        return self
+
+    def set_vector_store(self):
+        text = extract_text_from_PDF(files=self.files)
+        chunks = split_content_into_chunks(text)
+        self.vector_store = save_chunks_into_vectorstore(chunks, self.embedding)
+        return self
 
 
 def get_openai_model():
