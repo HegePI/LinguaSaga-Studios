78bfa00de34eb1b9972404ded131796a90382241
spoingus1 <barunes09@gmail.com>
2023-11-20 11:17:49 +0200

    Bump up game version to 2022.3.13f1

--- Game/Assets/Scripts/NPC1Behaviour.cs
+++ Game/Assets/Scripts/NPC1Behaviour.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Networking;
 using UnityEngine;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using TMPro;
 using UnityEngine.TextCore.Text;
 using System;
+using System.Threading;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
 
 public class NPC1Behaviour : MonoBehaviour
 {
@@ -13,9 +18,31 @@ public class NPC1Behaviour : MonoBehaviour
     public GameObject canvasObject;
     public GameObject canvasInteraction;
     public GameObject dialog;
+    private ClientWebSocket websocket;
+    private CancellationTokenSource cancellationTokenSource;
+    public string message;
 
     public TMP_InputField input;
 
+    async void Start()
+    {
+        //Initiate websocket for the NPC
+        cancellationTokenSource = new CancellationTokenSource();
+        websocket = new ClientWebSocket();
+
+        try
+        {
+            await websocket.ConnectAsync(new Uri("ws://127.0.0.1:8000/ws/conversation"), cancellationTokenSource.Token);
+            Debug.Log("WebSocket connected!");
+
+            //Initiate function to receive messages from websocket
+            _ = ReceiveMessages();
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("WebSocket connection error: " + e.Message);
+        }
+    }
     
     private void Update()
     {
@@ -29,6 +56,7 @@ public class NPC1Behaviour : MonoBehaviour
         if (Input.GetButtonDown("Interact") && isInTriggerRange)
         {
             canvasObject.SetActive(true);  
+
             input.onSubmit.AddListener(SendUserMessage);
         }
         else if (!isInTriggerRange || Input.GetButtonDown("Cancel")){
@@ -37,35 +65,71 @@ public class NPC1Behaviour : MonoBehaviour
         
     }
 
-    private void SendUserMessage(string arg0)
-    {   
-        input.Select();
-        print(input.text);
-        dialog.GetComponent<TMP_Text>().text += "\n\nPlayer: " + input.text;
-        input.text = "";
-    }
-    /*
-    The API for a simple get request. To be updated later once we have full code. For now, let's just print something out with a function "NPC Response"
-    IEnumerator FetchNPCResponse()
-    {
-        string url = "";
-        using (UnityWebRequest request = UnityWebRequest.Get(url))
+    private async void SendUserMessage(string arg0)
+    {   try
         {
-            yield return request.SendWebRequest();
-            if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+            input.Select();
+
+            //Send the input in the JSON format, store as a string, then converted to JSON
+            string jsonString = "{'player_name': 'player1','player_input':'" + input.text + "', 'npc_data': {'npc_name': 'Jhon Doe','backstory': '','description': '','tasks': [{'description': ''}]}}";
+            jsonString = jsonString.Replace('\'', '\"');
+            JObject jsonObject = JObject.Parse(jsonString);
+
+            //Function defined to send json
+            await SendJsonAsync(jsonObject);
+            
+            
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("WebSocket connection error: " + e.Message);
+        }
+    }
+
+    //Sends JSON through the websocket
+    private async Task SendJsonAsync(object data)
+    {
+        if (websocket.State != WebSocketState.Open)
+        {
+            Debug.LogError("WebSocket is not connected.");
+            return;
+        }
+
+        string json = JsonConvert.SerializeObject(data);
+        var encoded = Encoding.UTF8.GetBytes(json);
+        var buffer = new ArraySegment<Byte>(encoded, 0, encoded.Length);
+        await websocket.SendAsync(buffer, WebSocketMessageType.Text, true, cancellationTokenSource.Token);
+    }
+
+    private async Task ReceiveMessages()
+    {
+        var buffer = new byte[1024 * 4];
+
+        while (websocket.State == WebSocketState.Open)
+        {
+            try
             {
-                Debug.LogError(request.error);
+                var result = await websocket.ReceiveAsync(new ArraySegment<byte>(buffer), cancellationTokenSource.Token);
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await websocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, cancellationTokenSource.Token);
+                }
+                else
+                {
+                    message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+
+                    //Display message in canvas after receiving it from response
+                    dialog.GetComponent<TMP_Text>().text = "\n\n" + message + dialog.GetComponent<TMP_Text>().text;
+                    input.text = "";
+                }
             }
-            else
+            catch (Exception e)
             {
-                string responseData = request.downloadHandler.text;
-                JObject json = JObject.Parse(responseData);
-                Debug.Log("Hi! My name is " + json["first_name"].ToString() + " " + json["last_name"].ToString());
+                Debug.LogError("Error in receiving WebSocket message: " + e.Message);
+                break;
             }
         }
-        
     }
-    */
     private void OnTriggerStay2D(Collider2D Other){
         isInTriggerRange = true;
     }
